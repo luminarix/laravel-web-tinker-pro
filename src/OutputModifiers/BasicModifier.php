f4f6c06b024c4c7390d307068a967647922fc704
Mark <magyarheaven@gmail.com>
2025-08-10 06:09:41 +0200

    feat: update basic modifier

--- src/OutputModifiers/BasicModifier.php
+++ src/OutputModifiers/BasicModifier.php
@@ -6,22 +6,36 @@ final class BasicModifier implements OutputModifier
 {
     public function modify(mixed ...$args): string
     {
+        /** @var string $output */
         $output = $args['output'];
         /** @var int $runtime */
         $runtime = $args['runtime'];
+        /** @var int $memoryUsage */
         $memoryUsage = $args['memoryUsage'];
-        $timestamp = now()->format('Y-m-d H:i:s');
-        $formattedRuntime = number_format($runtime / 1_000, 3);
+
+        $memoryUsageFormatted = $this->formatBytes($memoryUsage);
+        $timestamp = now()->timestamp;
+        $formattedRuntime = round($runtime / 1_000, 3);
+        $outputSizeFormatted = $this->formatBytes(mb_strlen($output, 'binary'));
 
         $json = json_encode(
             value: compact(
                 'timestamp',
                 'formattedRuntime',
-                'memoryUsage',
-                'output'
+                'memoryUsageFormatted',
+                'output',
+                'outputSizeFormatted'
             )
         );
 
         return $json !== false ? $json : '';
+    }
+
+    public function formatBytes(int $bytes): string
+    {
+        $units = ['B', 'KB', 'MB', 'GB', 'TB'];
+        $power = $bytes > 0 ? floor(log($bytes, 1_024)) : 0;
+
+        return number_format($bytes / (1_024 ** $power), 2) . ' ' . $units[$power];
     }
 }
