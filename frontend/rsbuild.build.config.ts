31c54d8f46c221403a94aa397e7ea43774df1c97
Mark <magyarheaven@gmail.com>
2025-08-10 04:25:33 +0200

    fix: update paths

--- frontend/rsbuild.build.config.ts
+++ frontend/rsbuild.build.config.ts
@@ -2,56 +2,62 @@ import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
 
 export default defineConfig({
-  plugins: [pluginReact()],
-  tools: {
-    rspack: {
-      name: 'build',
-      target: 'web',
-      mode: 'production',
-      ignoreWarnings: [
-        // Suppress Monaco Editor dynamic require warnings
-        /Critical dependency: require function is used in a way in which dependencies cannot be statically extracted/,
-      ],
-    },
-  },
-  html: { template: './templates/layout.html' },
-  environments: {
-    local: {
-      output: {
-        legalComments: 'none',
-        assetPrefix: 'auto',
-        distPath: {
-          root: '../resources',
-          html: 'views',
+    plugins: [ pluginReact() ],
+    tools: {
+        rspack: {
+            name: 'build',
+            target: 'web',
+            mode: 'production',
+            ignoreWarnings: [
+                // Suppress Monaco Editor dynamic require warnings
+                /Critical dependency: require function is used in a way in which dependencies cannot be statically extracted/,
+            ],
         },
-        cleanDistPath: true,
-      },
-      tools: {
-        htmlPlugin(config) {
-          config.filename = 'local.blade.php'; // no index.html
-        },
-      },
-      html: { templateParameters: { prod: false } },
-      source: { define: { __PROD__: false } },
     },
+    html: { template: './templates/layout.html' },
+    environments: {
+        local: {
+            output: {
+                legalComments: 'none',
+                assetPrefix: 'auto',
+                distPath: {
+                    root: '../resources',
+                    css: 'dist/css',
+                    font: 'dist/font',
+                    js: 'dist/js',
+                    jsAsync: 'dist/js/async'
+                },
+                cleanDistPath: true,
+            },
+            tools: {
+                htmlPlugin(config) {
+                    config.filename = 'views/local.blade.php';
+                },
+            },
+            html: { templateParameters: { prod: false } },
+            source: { define: { __PROD__: false } },
+        },
 
-    production: {
-      output: {
-        legalComments: 'none',
-        assetPrefix: 'auto',
-        distPath: {
-          root: '../resources',
-          html: 'views',
+        production: {
+            output: {
+                legalComments: 'none',
+                assetPrefix: 'auto',
+                distPath: {
+                    root: '../resources',
+                    css: 'dist/css',
+                    font: 'dist/font',
+                    js: 'dist/js',
+                    jsAsync: 'dist/js/async'
+                },
+                cleanDistPath: true,
+            },
+            tools: {
+                htmlPlugin(config) {
+                    config.filename = 'views/production.blade.php';
+                },
+            },
+            html: { templateParameters: { prod: true } },
+            source: { define: { __PROD__: true } },
         },
-        cleanDistPath: true,
-      },
-      tools: {
-        htmlPlugin(config) {
-          config.filename = 'production.blade.php';
-        },
-      },
-      html: { templateParameters: { prod: true } },
-      source: { define: { __PROD__: true } },
     },
-  },
 });
---
+++ resources/views/production.blade.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html>
+<head>
+    <title>Laravel Web Tinker (production)</title>
+
+<meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><script defer src="../dist/js/lib-axios.5a2d5179.js"></script><script defer src="../dist/js/lib-react.366908d3.js"></script><script defer src="../dist/js/4146.ccb84095.js"></script><script defer src="../dist/js/index.fb175ea6.js"></script><link href="../dist/css/4146.99b73633.css" rel="stylesheet"><link href="../dist/css/index.54e20ad2.css" rel="stylesheet"></head>
+<body>
+<div id="root"></div>
+</body>
+</html>
