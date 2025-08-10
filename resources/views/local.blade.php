5d241319a30048c852bbe8c8ee040c39af89da8d
Mark <magyarheaven@gmail.com>
2025-08-10 13:41:18 +0200

    chore: add fresh build

--- resources/views/local.blade.php
+++ resources/views/local.blade.php
@@ -3,7 +3,7 @@
 <head>
     <title>Laravel Web Tinker (local)</title>
 
-<meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><script defer src="/vendor/web-tinker-pro/js/lib-axios.5a2d5179.js"></script><script defer src="/vendor/web-tinker-pro/js/lib-react.366908d3.js"></script><script defer src="/vendor/web-tinker-pro/js/4146.ccb84095.js"></script><script defer src="/vendor/web-tinker-pro/js/index.abcdbde9.js"></script><link href="/vendor/web-tinker-pro/css/4146.5070d8e0.css" rel="stylesheet"><link href="/vendor/web-tinker-pro/css/index.54e20ad2.css" rel="stylesheet"></head>
+<meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><script defer src="/vendor/web-tinker-pro/js/lib-axios.5a2d5179.js"></script><script defer src="/vendor/web-tinker-pro/js/lib-react.366908d3.js"></script><script defer src="/vendor/web-tinker-pro/js/1498.08714c23.js"></script><script defer src="/vendor/web-tinker-pro/js/index.99dd872e.js"></script><link href="/vendor/web-tinker-pro/css/1498.5070d8e0.css" rel="stylesheet"><link href="/vendor/web-tinker-pro/css/index.60398fe5.css" rel="stylesheet"></head>
 <body>
 <div id="root"></div>
 </body>
